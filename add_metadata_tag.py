996699e5e30f609d60874b1e800dd05885d524a3
Therhaie <theo.thuillier@uni.lu>
2026-04-02 18:40:28 +0200

    rotation_experiment_copy.py perform the rotation experiment with either the targeted_chunk or the 'reached_chunk'. However if working with 'reached_chunk' necessary to ensure that no chunk sufer from a double rotation and remove them from the list of reached chunk before and to not false the experiment results

--- add_metadata_tag.py
+++ add_metadata_tag.py
@@ -35,6 +35,7 @@ with open("documents_RAGBench/merged_id_triplets_no_duplicates.json", encoding="
 
 list_append_chunk = []
 t0 = time.time()
+# To append "targeted_chunk" to the json file for the "normal experiment scenario" 
 while len(list_append_chunk) < len(list_identifier):
     print(f"Processing... {len(list_append_chunk)}/{len(list_identifier)} triplet indices assigned with metadata tags.")
     print(f"Time elapsed: {time.time() - t0:.2f} seconds")
@@ -73,8 +74,50 @@ while len(list_append_chunk) < len(list_identifier):
                 if chunk_identifier == identifier_sentence:
                     break # break this loop to only add the first best chunk for each query through this loop
 
+# To append "reached_chunk" to the json file for the "optimal scenario", append all the chunks reach by a query  
+
+for entries in ground_truth_data:
+
+    triplet_index_orig_query = entries.get("triplet_index")
+
+    for entry in entries["stable_chunks"]:
+
+        triplet_index = entry.get("triplet_index")
+        document_id = entry.get("document_id")
+        phrase_seq = entry.get("phrase_seq")
+
+        chunk_identifier = f"{triplet_index}|{document_id}{phrase_seq}"
+
+        # if "reached_chunk" not in entries:
+        #     entries["reached_chunk"] = []
+        # entries["reached_chunk"].append(chunk_identifier)
+
+        # need to find the query associated in merged_data with the same triplet_index and add chunk_identifier
+        for chunk in merged_data:
+            if chunk.get("id_triplets") == triplet_index_orig_query:     
+                if "reached_chunk" not in chunk:
+                    chunk["reached_chunk"] = []
+                chunk["reached_chunk"].append(chunk_identifier)
+                break
+                
+
+        #         break  # Stop after finding the first match
+
+
+        # for doc in merged_data:
+        #     doc_triplet_index = doc.get("id_triplets")
+        #     for sentence in doc.get("sentences", []):
+        #         identifier_sentence = f"{doc_triplet_index}|{sentence[0]}"
+        #         if chunk_identifier == identifier_sentence:
+        #             sentence.append(doc.get("id_triplets"))
+        #             break  # Stop after finding the first 
+        #     if chunk_identifier == identifier_sentence:
+        #         break # break the second for loop
+
+
+
 # After all processing and modifications are complete, save to a new file
-with open("documents_RAGBench/merged_id_triplets_with_metadata.json", "w", encoding="utf-8") as out_fh:
+with open("documents_RAGBench/merged_id_triplets_with_metadata2.json", "w", encoding="utf-8") as out_fh:
     json.dump(merged_data, out_fh, ensure_ascii=False, indent=2)
 
 print("Modifications saved to documents_RAGBench/merged_id_triplets_with_metadata.json")
